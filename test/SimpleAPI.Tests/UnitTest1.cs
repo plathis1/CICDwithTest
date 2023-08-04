690ab4963be13e4fb49c940fb4b1652d4974a9f0
plathis1 <plathis1@binghamton.edu>
2023-08-03 17:29:08 -0700

    Release

--- test/SimpleAPI.Tests/UnitTest1.cs
+++ test/SimpleAPI.Tests/UnitTest1.cs
@@ -13,6 +13,7 @@ public class UnitTest1
 {    
     private readonly Mock<ILogger<WeatherForecastController>> _logger;
     private readonly Mock<WeatherDbContext> _dbContext;
+    private readonly Mock<WeatherForecastController> waetherController;
     public static List<WeatherForecast> GetFakeEmployeeList()
     {
         return new List<WeatherForecast>()
@@ -27,6 +28,7 @@ public class UnitTest1
     {
         this._logger = new Mock<ILogger<WeatherForecastController>>();
         this._dbContext = new Mock<WeatherDbContext>();
+        this.waetherController = new Mock<WeatherForecastController>(this._logger.Object, this._dbContext.Object);
     }
 
     [Fact]
@@ -49,6 +51,26 @@ public class UnitTest1
         Assert.Equal(sampleData.Count, result?.Count());
     }
 
+    [Fact]
+    public async Task Test1_put()
+    {
+        var sampleData = new List<WeatherForecast>()
+        {
+            new WeatherForecast { Id = 1, TemperatureC = 90, Summary = "Hot" , Date = DateTime.Now},
+            new WeatherForecast { Id = 2, TemperatureC = 39, Summary = "cold" , Date = DateTime.Now},
+            new WeatherForecast { Id = 3, TemperatureC = 75, Summary = "moderate" , Date = DateTime.Now}
+        };
 
+        // Set up the DbSet mock to return the sample data when queried
+        this._dbContext.Setup<DbSet<WeatherForecast>>(db => db.WeatherForecasts).ReturnsDbSet(sampleData);
+        WeatherForecast sample_put= new() { TemperatureC = 43, Summary = " Second cold" , Date = DateTime.Now};
+
+
+        var controller = new WeatherForecastController(this._logger.Object, this._dbContext.Object);
+        IActionResult result = await controller.Put(2,sample_put);
+
+        // Assert
+        Assert.Equal(typeof(BadRequestResult), result.GetType());
+    }
     
 }

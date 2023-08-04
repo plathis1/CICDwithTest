a4f967b1e8bdc3c9463737c896a1c44bfba6cfcb
plathis1 <plathis1@binghamton.edu>
2023-08-03 18:40:15 -0700

    endpoint fix

--- src/SimpleAPI/Program.cs
+++ src/SimpleAPI/Program.cs
@@ -1,11 +1,16 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using SimpleAPI.Models;
 
+var builder = WebApplication.CreateBuilder(args);
+ConfigurationManager configurationManager = builder.Configuration;
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<WeatherDbContext>(opt => opt.UseSqlServer(configurationManager.GetConnectionString("WeatherConnection")));
+
 
 var app = builder.Build();
 
@@ -21,5 +26,12 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapPost("weatherforecast", async (WeatherForecast weatherForecast, WeatherDbContext dbContext) =>
+{
+    dbContext.WeatherForecasts.Add(weatherForecast);
+    await dbContext.SaveChangesAsync();
+    return Results.CreatedAtRoute("GetWeatherForecast", new { id = weatherForecast.Id }, weatherForecast);
+}).Produces<WeatherForecast>(StatusCodes.Status201Created);
 
 app.Run();
